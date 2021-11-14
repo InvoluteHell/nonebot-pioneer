235b40fa0f8fa3dd896fa7ac8baf76c159974689
Handiwork <johnson112009@live.cn>
2021-11-14 23:00:10 +0800

    add flake8-black plugin; add dependency-injector framework;

--- nonebot_pioneer/plugins/bbot/__init__.py
+++ nonebot_pioneer/plugins/bbot/__init__.py
@@ -1,4 +1,14 @@
+from dependency_injector import containers, providers
+
 from .android import Android
 
 
-android = Android("meo")
+class PluginContainer(containers.DeclarativeContainer):
+    config = providers.Configuration()
+    android = providers.Singleton(Android, config.name)
+
+
+container = PluginContainer()
+container.config.from_dict({"name": "android"})
+android = container.android()
+android.listen()
--- nonebot_pioneer/plugins/bbot/android.py
+++ nonebot_pioneer/plugins/bbot/android.py
@@ -1,7 +1,6 @@
 from nonebot.adapters import Bot
 from nonebot.matcher import Matcher
 from nonebot.plugin import on_command
-from nonebot.rule import to_me
 
 
 class Android:
@@ -9,7 +8,8 @@ class Android:
         self.version = "0.0.1"
         self.name = name
 
-        on_command("version", rule=to_me(), priority=5, handlers=[self.handle_version])
+    def listen(self):
+        on_command("version", handlers=[self.handle_version])
         on_command("name", handlers=[self.handle_name])
 
     async def handle_version(self, bot: Bot, matcher: Matcher):
